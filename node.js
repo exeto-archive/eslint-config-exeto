470e5878eeab4e1020e96a304fb91d6dd4093b78
Timofey Dergachev <t@exeto.me>
2016-04-22 11:54:34 +0300

    Fix "strict" rule in the config for node

--- node.js
+++ node.js
@@ -1,10 +1,14 @@
 module.exports = {
   extends: require.resolve('eslint-config-airbnb/base'),
+  parserOptions: {
+    sourceType: 'script'
+  },
   rules: {
     'func-names': 0,
     'no-param-reassign': 0,
     'consistent-return': 0,
     'no-shadow': [2, { 'allow': ['err', 'resolve', 'reject', 'cb'] }],
     'no-console': 0,
+    'strict': [2, 'global'],
   },
 };
